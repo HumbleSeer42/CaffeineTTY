2e4d96c9e58a3a057752228b33903e6ac6047e44
HumbleSeer42 <jp01220414@icloud.com>
2026-09-07 21:20:00 -0400

    feat: add first render scene

--- app/src/main/java/self/humbleseer/Caffeinetty.java
+++ app/src/main/java/self/humbleseer/Caffeinetty.java
@@ -3,12 +3,22 @@ package self.humbleseer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 
 public class Caffeinetty extends Application {
   @Override
   public void start(Stage stage) {
-    Group root = new Group();
+    Text first_line = new Text(10, 20, "This is a test.");
+    first_line.setFont(new Font(12));
+    first_line.setFill(Color.RED);
+    Text second_line = new Text(10, 40, "This is a second line of text");
+    second_line.setFont(new Font(12));
+    second_line.setFill(Color.BLUE);
+
+    Group root = new Group(first_line, second_line);
     Scene scene = new Scene(root, 500, 500);
 
     stage.setTitle("CaffeineTTY");
