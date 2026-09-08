b7918cb3a8b674a0b4bf16d662fad0ab19e88e51
HumbleSeer42 <jp01220414@icloud.com>
2026-09-07 21:46:37 -0400

    refactor: remove needless cruft

--- app/src/main/java/self/humbleseer/Caffeinetty.java
+++ app/src/main/java/self/humbleseer/Caffeinetty.java
@@ -4,29 +4,19 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
 import javafx.scene.control.TextField;
 
 public class Caffeinetty extends Application {
   @Override
   public void start(Stage stage) {
-    Text first_line = new Text(10, 20, "This is a test.");
-    first_line.setFont(new Font(12));
-    first_line.setFill(Color.RED);
-    Text second_line = new Text(10, 40, "This is a second line of text");
-    second_line.setFont(new Font(12));
-    second_line.setFill(Color.BLUE);
-
     TextField myFirstField = new TextField();
     myFirstField.setMinWidth(500);
     myFirstField.setMinHeight(500);
     myFirstField.setMaxSize(500, 500);
     myFirstField.setAlignment(Pos.TOP_LEFT);
 
-    Group root = new Group(first_line, second_line, myFirstField);
+    Group root = new Group(myFirstField);
     Scene scene = new Scene(root, 500, 500);
 
     stage.setTitle("CaffeineTTY");
