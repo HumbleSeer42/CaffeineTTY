7cb95eace9201f677c1e13d563a859e6c1ae147a
HumbleSeer42 <jp01220414@icloud.com>
2026-09-12 11:04:33 -0400

    feat(terminal): embed JediTerm with SwingNode

--- app/src/main/java/self/humbleseer/Caffeinetty.java
+++ app/src/main/java/self/humbleseer/Caffeinetty.java
@@ -6,26 +6,46 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+import com.jediterm.terminal.ui.JediTermWidget;
+import com.jediterm.terminal.ui.settings.DefaultSettingsProvider;
+import javafx.embed.swing.SwingNode;
+import javax.swing.SwingUtilities;
 
 public class Caffeinetty extends Application {
   @Override
   public void start(Stage stage) {
-    TextField myFirstField = new TextField();
-    myFirstField.setMinWidth(500);
-    myFirstField.setMinHeight(500);
-    myFirstField.setMaxSize(500, 500);
-    myFirstField.setAlignment(Pos.TOP_LEFT);
-    myFirstField.setOnAction(event -> {
-      System.out.println(myFirstField.getText());
-      myFirstField.clear();
-    });
+    /*
+     * TextField myFirstField = new TextField();
+     * myFirstField.setMinWidth(500);
+     * myFirstField.setMinHeight(500);
+     * myFirstField.setMaxSize(500, 500);
+     * myFirstField.setAlignment(Pos.TOP_LEFT);
+     * myFirstField.setOnAction(event -> {
+     * System.out.println(myFirstField.getText());
+     * myFirstField.clear();
+     * });
+     */
 
-    Group root = new Group(myFirstField);
+    SwingNode terminalContainer = new SwingNode();
+    BorderPane root = new BorderPane();
+    root.setCenter(terminalContainer);
+
     Scene scene = new Scene(root, 500, 500);
 
     stage.setTitle("CaffeineTTY");
     stage.setScene(scene);
     stage.setResizable(false);
     stage.show();
+
+    SwingUtilities.invokeLater(() -> {
+      JediTermWidget term = new JediTermWidget(
+          80,
+          14,
+          new DefaultSettingsProvider());
+
+      terminalContainer.setContent(term);
+    });
   }
 }
